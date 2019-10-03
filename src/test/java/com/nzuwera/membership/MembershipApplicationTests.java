b6b7e8aff3ee60c385f6cc7b99cd9cc13e939953
nzuwera <nzuwera2002@gmail.com>
2019-10-03 13:33:52 +0200

    Code cleanup based on SonarLint. Code cleanup and restructuring

--- src/test/java/com/nzuwera/membership/MembershipApplicationTests.java
+++ src/test/java/com/nzuwera/membership/MembershipApplicationTests.java
@@ -30,6 +30,7 @@ import static org.junit.Assert.assertEquals;
 public class MembershipApplicationTests {
     private static final String DATE_PATTERN = "yyyy-MM-dd";
     private static final UUID planId = UUID.randomUUID();
+    private static final String plan001 = "Plan001";
     private Plan plan;
 
     @Autowired
@@ -51,32 +52,33 @@ public class MembershipApplicationTests {
      */
     @Test
     public void test_001_createPlan() throws AlreadyExistsException {
-        Plan createdPlan = planService.createPlan(plan).getData();
-        Assert.assertEquals("CreatedPlan", createdPlan.getName(), "Plan001");
+        Plan createdPlan = (Plan)planService.createPlan(plan).getData();
+        Assert.assertEquals("CreatedPlan", plan001, createdPlan.getName());
     }
 
     /**
      * Positive test of updating a plan
      *
-     * @throws Exception
+     * @throws Exception Exception
      */
     @Test
     public void test_002_updatePlan() throws Exception {
         Date startDate = new Date();
         Date updatedEndDate = Utils.setEndDate(startDate, 40);
-        ResponseObject<Plan> plan001 = planService.getPlanByName("Plan001");
-        Plan plan = plan001.getData();
+        ResponseObject responseObject = planService.getPlanByName(plan001);
+        Plan plan = (Plan) responseObject.getData();
         plan.setEndDate(updatedEndDate);
         planService.updatePlan(plan);
-        ResponseObject<Plan> updatedPlan = planService.getPlanByName("Plan001");
-        assertEqualDates(updatedPlan.getData().getEndDate(), updatedEndDate);
+        ResponseObject updatedPlan = planService.getPlanByName(plan001);
+        Plan plan1 = (Plan)updatedPlan.getData();
+        assertEqualDates(plan1.getEndDate(), updatedEndDate);
     }
 
     /**
      * Method to check if two dates are equal
      *
-     * @param expected
-     * @param value
+     * @param expected expected date
+     * @param value date value to be tested
      */
     private static void assertEqualDates(Date expected, Date value) {
         DateFormat formatter = new SimpleDateFormat(DATE_PATTERN);
@@ -91,7 +93,7 @@ public class MembershipApplicationTests {
     @Test
     public void test_999_deletePlan() throws NotFoundException {
         planService.deletePlan(plan);
-        ResponseObject<Plan> deletedPlan = planService.getPlanByName("Plan001");
-        Assert.assertEquals("deletePlan",new Plan(),deletedPlan.getData());
+        ResponseObject deletedPlan = planService.getPlanByName(plan001);
+        Assert.assertEquals("deletePlan", new Plan(), deletedPlan.getData());
     }
 }
